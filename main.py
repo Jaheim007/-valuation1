c9df15d7d59f1f84b81e1444c981b1812a21876a
iMac-13 <imac-13@iMac-de-iMac-13.local>
2022-01-07 12:53:42 +0000

    Evaluation1

---
+++ main.py
@@ -0,0 +1,14 @@
+#Exo 1
+def extrait_ensemble_des_voyelles(mot):
+    personne = ("alice", "jean", "pierre", "jack")
+    return personne
+
+personnes = {
+    "1": extrait_ensemble_des_voyelles
+}
+
+personnes[0]
+#Exo 2
+def transforme_en_numbros(mot):
+    numbros={"alice", "jean", "pierre", "jack"}
+    return numbros
